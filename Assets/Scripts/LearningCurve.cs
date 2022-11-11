d2229209c93fd9d23b20d2b3d128098c2a792a3e
SYmpHony <hfsymphony@gmail.com>
2022-11-11 16:52:35 +0800

    Upload

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int currentAge = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(30 + 1);
+        Debug.Log(currentAge + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
