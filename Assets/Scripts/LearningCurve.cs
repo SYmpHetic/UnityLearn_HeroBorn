deffff1a46fd737fa17ec78a5f89f97b59f1625e
SYmpHony <hfsymphony@gmail.com>
2022-11-13 17:44:16 +0800

    11/13 Update P93

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class LearningCurve : MonoBehaviour
 {
     public int currentAge = 30;
+    public string Firstname = "Harrison";
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(30 + 1);
-        Debug.Log(currentAge + 1);
+        int characterLevel = 31;
+        Debug.Log("Choose a character");
+        GenerateCharacter("Spike", characterLevel);
+        Debug.Log("Nice choice");
     }
 
     // Update is called once per frame
+    /// <summary>
+    /// 
+    /// </summary>
     void Update()
     {
         
+    }
+
+    public void GenerateCharacter(string name, int level)
+    {
+
+        Debug.LogFormat("Character:{0} - Level:{1}", name, level);
     }
 }
